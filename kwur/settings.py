930a5fa5884c204fc73e169e5912f3b25391cdfc
gracewylu <lu.grace.wy@gmail.com>
2016-09-20 23:14:58 -0500

    attempt to fix allowed_hosts

--- kwur/settings.py
+++ kwur/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
