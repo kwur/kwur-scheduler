e83c95886767a65c6eb069beb5dda65ce0c52a5d
gracewylu <lu.grace.wy@gmail.com>
2016-09-20 20:40:06 -0500

    implemented secret keys

--- kwur/settings.py
+++ kwur/settings.py
@@ -18,11 +18,16 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'kwur.herokuapp.com']
 
 
 # Application definition
