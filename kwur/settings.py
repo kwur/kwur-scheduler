20b1ec3965f811c7ae6d3b83b70649228b4e64ed
gracewylu <lu.grace.wy@gmail.com>
2016-09-20 21:07:44 -0500

    added https

--- kwur/settings.py
+++ kwur/settings.py
@@ -18,6 +18,8 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
