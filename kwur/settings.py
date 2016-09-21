d867af1640a249803dea588de7c0e56a7dccb5a7
gracewylu <lu.grace.wy@gmail.com>
2016-09-20 23:19:09 -0500

    y you no work

--- kwur/settings.py
+++ kwur/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['kwur.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
