2e0c25183df493188903421f821e4f41653fda9c
gracewylu <lu.grace.wy@gmail.com>
2017-01-30 00:10:21 -0600

    fixed ascii issue hopefully

--- scheduler/models.py
+++ scheduler/models.py
@@ -48,7 +48,7 @@ class Choice(models.Model):
 
 	def __str__(self):
 		format = '%I:%M %p'
-		return self.show.show_name + '-' + str(self.show.dj) + '-' + DAYS_OF_WEEK[self.day] + '-' + self.time.strftime(format)
+		return self.show.show_name + '-' + unicode(self.show.dj) + '-' + DAYS_OF_WEEK[self.day] + '-' + self.time.strftime(format)
 
 
 class Crediting(models.Model):
