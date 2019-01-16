5bab8ba0b54324858b08130819a7fd0d235de184
Ethan Jaynes <ethandjay@gmail.com>
2019-01-15 20:27:46 -0600

    raised DATA_UPLOAD_MAX_NUMBER_FIELDS limit

--- kwur/settings.py
+++ kwur/settings.py
@@ -14,6 +14,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import dj_database_url
 
+# Adding this because we were hitting our limit, might be hacky?
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 100000
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
