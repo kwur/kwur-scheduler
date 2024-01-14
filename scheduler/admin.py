39da6249d0c89905aef46eccb986f898f0c27d39
victoriaprisco <p.victoria@wustl.edu>
2024-01-14 13:29:51 -0500

    ugh

--- scheduler/admin.py
+++ scheduler/admin.py
@@ -8,29 +8,26 @@ from .models import BaseUser, Show, Choice, Crediting
 
 class BaseUserResource(resources.ModelResource):
 
-		class Meta:
-			model = BaseUser
-			fields = ('id', 'first_name', 'last_name', 'credits',)
+	class Meta:
+		model = BaseUser
+		fields = ('id', 'first_name', 'last_name', 'credits',)
 
 class ChoiceResource(resources.ModelResource):
-
-		class Meta:
-			model = Choice
+	class Meta:
+		model = Choice
 
 class BaseUserAdmin(ImportExportMixin, admin.ModelAdmin):
     resource_class = BaseUserResource
 
 class ShowAdmin(ImportExportMixin, admin.ModelAdmin):
-	
-		class Meta:
-			model = Show 
-			fields = ('id', 'show_name', 'dj', 'co_dj', 'genre', 'tagline', 'day', 'time', 'timestamp')
+	class Meta:
+		model = Show 
+		fields = ('id', 'show_name', 'dj', 'co_dj', 'genre', 'tagline', 'day', 'time', 'timestamp')
 
 class ChoiceAdmin(ImportExportMixin, admin.ModelAdmin):
-    	
-    	class Meta:
-    		model = Choice
-    		fields = ('id', 'show', 'choice_num', 'day', 'time', 'not_available')
+	class Meta:
+		model = Choice
+		fields = ('id', 'show', 'choice_num', 'day', 'time', 'not_available')
 
 class CreditingAdmin(ImportExportMixin, admin.ModelAdmin):
 
