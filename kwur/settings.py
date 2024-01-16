63dfd34f2b12782d700828f82150ea3a22eaffae
victoriaprisco <p.victoria@wustl.edu>
2024-01-16 15:10:57 -0600

    allowed hosts

--- kwur/settings.py
+++ kwur/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kwur.herokuapp.com']
 
 
 # Application definition
